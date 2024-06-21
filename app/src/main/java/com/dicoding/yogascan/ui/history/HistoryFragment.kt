e160712d96c3c2b2f99106021f7a36c11192a2b3
Maitsa Adinda Prana Putri <112807546+maitsaadinda@users.noreply.github.com>
2024-06-21 19:45:52 +0700

    Update HistoryFragment.kt

--- app/src/main/java/com/dicoding/yogascan/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/dicoding/yogascan/ui/history/HistoryFragment.kt
@@ -1,21 +1,18 @@
 package com.dicoding.yogascan.ui.history
 
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.dicoding.yogascan.R
 import com.dicoding.yogascan.ViewModelFactory
 import com.dicoding.yogascan.adapter.HistoryAdapter
 import com.dicoding.yogascan.data.ResultState
-import com.dicoding.yogascan.data.response.HistoryItem
 import com.dicoding.yogascan.databinding.FragmentHistoryBinding
 
 class HistoryFragment : Fragment() {
@@ -36,25 +33,26 @@ class HistoryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Setup RecyclerView
         val historyAdapter = HistoryAdapter()
         binding.rvItemPose.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = historyAdapter
         }
 
-        // Observe ViewModel untuk mendapatkan data history
-        observeViewModel()
+        historyViewModel.getSession().observe(viewLifecycleOwner){ user ->
+            Log.d("History", user.uid)
+            observeViewModel(user.uid)
+        }
     }
 
-    private fun observeViewModel() {
-        historyViewModel.historyLiveData.observe(viewLifecycleOwner, Observer { result ->
+    private fun observeViewModel(uid: String) {
+        historyViewModel.getHistory(uid).observe(viewLifecycleOwner, Observer { result ->
             when (result) {
                 is ResultState.Loading -> showLoading(true)
                 is ResultState.Success -> {
                     showLoading(false)
-                    result.data?.let { historyResponse ->
-                        val historyList = historyResponse.history // Ambil list history dari response
+                    result.data.let { historyResponse ->
+                        val historyList = historyResponse.history
                         (binding.rvItemPose.adapter as? HistoryAdapter)?.submitList(historyList)
                     }
                 }
