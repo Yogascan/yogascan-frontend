bf9c01969e766bd645bfc9aae54554770c456e7d
Maitsa Adinda Prana Putri <112807546+maitsaadinda@users.noreply.github.com>
2024-06-21 19:49:46 +0700

    Update ProfileViewModel.kt

--- app/src/main/java/com/dicoding/yogascan/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/dicoding/yogascan/ui/profile/ProfileViewModel.kt
@@ -2,13 +2,13 @@ package com.dicoding.yogascan.ui.profile
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.dicoding.yogascan.data.ResultState
 import com.dicoding.yogascan.data.repository.UserRepository
-import com.dicoding.yogascan.data.response.ProfileResponse
+import com.dicoding.yogascan.data.response.Profile
 import com.dicoding.yogascan.data.response.SigninResponse
 import kotlinx.coroutines.launch
+import java.io.File
 
 class ProfileViewModel(private val userRepository: UserRepository) : ViewModel() {
     fun logout() {
@@ -16,12 +16,10 @@ class ProfileViewModel(private val userRepository: UserRepository) : ViewModel()
             userRepository.logout()
         }
     }
+    fun getProfile(uid: String): LiveData<ResultState<Profile>> = userRepository.getProfile(uid)
 
-    fun getProfile(uid: String): LiveData<ResultState<ProfileResponse>> {
-        return userRepository.getProfile(uid)
-    }
+    fun getSession(): LiveData<SigninResponse> = userRepository.getSession()
 
-    fun getSession(): LiveData<SigninResponse> {
-        return userRepository.getSession().asLiveData()
-    }
+    fun updateProfilePicture(image: File, uid: String) = userRepository.updateProfilePicture(image, uid)
+
 }
