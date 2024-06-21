6f34ef4a37fe24dfc49215a42aeff88ea968a4d2
Maitsa Adinda Prana Putri <112807546+maitsaadinda@users.noreply.github.com>
2024-06-21 19:43:47 +0700

    Update DetailViewModel.kt

--- app/src/main/java/com/dicoding/yogascan/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/dicoding/yogascan/ui/detail/DetailViewModel.kt
@@ -3,24 +3,19 @@ package com.dicoding.yogascan.ui.detail
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.dicoding.yogascan.data.repository.PoseRepository
-import kotlinx.coroutines.launch
+import com.dicoding.yogascan.data.response.SigninResponse
 
 class DetailViewModel (private val poseRepository: PoseRepository) : ViewModel(){
     private val _favoriteStatus = MutableLiveData<Boolean>()
-    val favoriteStatus: LiveData<Boolean> = _favoriteStatus
 
     fun getDetailPose(id: String?) = poseRepository.getDetailPose(id)
 
-    fun addFavoritePose(poseId: String) {
-        viewModelScope.launch {
-            try {
-                poseRepository.addFavoritePose(poseId)
-                _favoriteStatus.value = true
-            } catch (e: Exception) {
-                _favoriteStatus.value = false
-            }
-        }
-    }
+    fun getSession() : LiveData<SigninResponse> = poseRepository.getSession()
+
+    fun getUserFavorites(userId: String) = poseRepository.getFavorite(userId)
+
+    fun deleteUserFavorite(userId: String, poseId: String) = poseRepository.deleteFavoritePose(userId, poseId)
+
+    fun addFavoritePose(userId:String, poseId: String) = poseRepository.addFavoritePose(userId, poseId)
 }
