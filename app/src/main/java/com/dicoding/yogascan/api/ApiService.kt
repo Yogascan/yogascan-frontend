1734b4a9be28d6a346d1cf48a44d232cca845e13
Maitsa Adinda Prana Putri <112807546+maitsaadinda@users.noreply.github.com>
2024-06-21 19:30:42 +0700

    Update ApiService.kt

--- app/src/main/java/com/dicoding/yogascan/api/ApiService.kt
+++ app/src/main/java/com/dicoding/yogascan/api/ApiService.kt
@@ -2,22 +2,26 @@ package com.dicoding.yogascan.api
 
 import com.dicoding.yogascan.data.LoginRequest
 import com.dicoding.yogascan.data.SignupRequest
+import com.dicoding.yogascan.data.response.CommonRequestBody
+import com.dicoding.yogascan.data.response.CommonFavoriteResponseBody
 import com.dicoding.yogascan.data.response.DetailResponse
 import com.dicoding.yogascan.data.response.FavoriteResponse
-import com.dicoding.yogascan.data.response.FavoritesItem
-import com.dicoding.yogascan.data.response.HistoryItem
+import com.dicoding.yogascan.data.response.CommonUidRequestBody
+import com.dicoding.yogascan.data.response.HistoryRequest
 import com.dicoding.yogascan.data.response.HistoryResponse
 import com.dicoding.yogascan.data.response.PoseResponse
+import com.dicoding.yogascan.data.response.PosesPredictionPostData
 import com.dicoding.yogascan.data.response.PredictionResponse
-import com.dicoding.yogascan.data.response.ProfileResponse
+import com.dicoding.yogascan.data.response.Profile
 import com.dicoding.yogascan.data.response.SigninResponse
 import com.dicoding.yogascan.data.response.SignupResponse
+import com.dicoding.yogascan.data.response.UpdatePictureResponse
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.http.Body
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.HTTP
+import retrofit2.http.Headers
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
@@ -42,22 +46,16 @@ interface ApiService {
         @Part image: MultipartBody.Part,
     ): PredictionResponse
 
-    @POST("saveHistory")
-    suspend fun saveHistory(@Body historyItem: HistoryItem)
-
-    @GET("history")
+    @POST("history")
+    suspend fun saveHistory(
+        @Body predictionPostData: PosesPredictionPostData)
+    
+    @Headers("Content-Type: application/json")
+    @POST("histories")
     suspend fun getHistory(
-        @Path("uid") uid: String?
+        @Body request: HistoryRequest
     ): HistoryResponse
-
-    @Multipart
-    @POST("uploadImage")
-    suspend fun saveImageToServer(
-        @Part image: MultipartBody.Part,
-        @Part("prediction") prediction: RequestBody,
-        @Part("confidence") confidence: RequestBody
-    ):HistoryResponse
-
+    
     @POST("login")
     suspend fun login(
         @Body request: LoginRequest
@@ -68,33 +66,30 @@ interface ApiService {
         @Body request: SignupRequest
     ): SignupResponse
 
-//    @FormUrlEncoded
-//    @POST("login")
-//    suspend fun login(
-//        @Field("email") email: String,
-//        @Field("password") password: String
-//    ): SigninResponse
-//
-//
-//    @FormUrlEncoded
-//    @POST("signup")
-//    suspend fun signup(
-//        @Field("username")username: String,
-//        @Field("email") email: String,
-//        @Field("password") password: String,
-//    ): SignupResponse
-
     @POST("user")
-    fun getProfile(
-        @Field("uid") uid: String,
-    ): ProfileResponse
+    suspend fun getProfile(
+        @Body uid: CommonUidRequestBody,
+    ): Profile
 
     @POST("favorite")
     suspend fun postFavorite(
-        @Body favoritesItem: FavoritesItem
+        @Body favoritesItem: CommonRequestBody
+    ): CommonFavoriteResponseBody
+
+    @HTTP(method = "DELETE", path = "favorite", hasBody = true)
+    suspend fun deleteFavoritePoses(
+        @Body favoritesItem: CommonRequestBody
+    ) : CommonFavoriteResponseBody
+
+    @POST("favorites")
+    suspend fun getFavoritePoses(
+        @Body getFavorite: CommonUidRequestBody
     ): FavoriteResponse
 
-    @GET("favorite")
-    suspend fun getFavoritePoses(
-    ): FavoriteResponse
+    @Multipart
+    @POST("update-picture")
+    suspend fun setProfilePicture(
+        @Part("uid") uid: RequestBody,
+        @Part image: MultipartBody.Part
+    ) : UpdatePictureResponse
 }
